d5b6264b6ed3b73241209cd60c51fc4b3af43e80
Master <master@srvuisrael.edu.ec>
2024-01-08 19:24:05 -0500

    examen

---
+++ dAstudilloExamen/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace dAstudilloExamen
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ dAstudilloExamen/VIstas/Login.xaml.cs
@@ -0,0 +1,9 @@
+namespace dAstudilloExamen.VIstas;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+}
